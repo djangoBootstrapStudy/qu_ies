25d8998c78520e96798108d117c0c19663b3929f
halmango <kimby0709@naver.com>
2022-06-02 08:42:28 +0900

    Feat: 추가 author, title 모델

--- my_quiz/models.py
+++ my_quiz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Quiz(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # 사용자의 계정이 삭제되면 퀴즈도 삭제됨
+    title = models.CharField(max_length=50) # 최대 길이 50
+
+    class Meta:
+        verbose_name_plural = 'Quizzes' # 정확한 복수으로 수정
