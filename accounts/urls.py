5926f17a8cd6e3ef108df88a60381d29f28037f2
halmango <kimby0709@naver.com>
2022-06-02 21:25:28 +0900

    Style: 코드 포맷 변경, 코드 수정 없음

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SignUp
 
 urlpatterns = [
-    path('signup/', SignUp.as_view(), name='signup'),
+    path("signup/", SignUp.as_view(), name="signup"),
 ]
