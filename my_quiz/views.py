04021e58c466cb71a916cc9f9744e2212c04f9cb
halmango <kimby0709@naver.com>
2022-06-02 16:38:29 +0900

    Feat: 추가 MyQuizList 클래스 My Quiz 페이지에서 퀴즈 목록을 보여줌

--- my_quiz/views.py
+++ my_quiz/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Quiz
+
 
 # Create your views here.
+class MyQuizList(ListView):
+    model = Quiz
+
