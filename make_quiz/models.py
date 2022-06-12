50e9684cfd1bab5fc605d5fb4cdb239b965bdbdf
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-13 00:42:18 +0900

    Refactor: __str__ 정의

--- make_quiz/models.py
+++ make_quiz/models.py
@@ -3,14 +3,19 @@ from my_quiz.models import Quiz
 
 # Create your models here.
 class QuizQuestion(models.Model):
-    quiz=models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    quiz=models.ForeignKey(Quiz,verbose_name="퀴즈",on_delete=models.CASCADE)
     no=models.IntegerField("문제번호")
-    content=models.CharField(max_length=250)
+    content=models.CharField("문제내용",max_length=250)
     answer = models.IntegerField("문제해답")
+
+    def __str__(self):
+        return f"문제{self.no}번"
 
 
 class QuizExample(models.Model):
-    question=models.ForeignKey(QuizQuestion,on_delete=models.CASCADE)
+    question=models.ForeignKey(QuizQuestion,verbose_name="문제",on_delete=models.CASCADE)
     no=models.IntegerField("보기번호")
-    content=models.CharField(max_length=250)
+    content=models.CharField("보기내용",max_length=250)
 
+    def __str__(self):
+        return f"{self.question}-보기({self.no})"
