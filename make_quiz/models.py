b42e06ce410fabe6246083b26973e1374acfaa70
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-13 00:20:56 +0900

    Feat: 퀴즈문제&보기 모델 생성

--- make_quiz/models.py
+++ make_quiz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from my_quiz.models import Quiz
 
 # Create your models here.
+class QuizQuestion(models.Model):
+    quiz=models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    no=models.IntegerField("문제번호")
+    content=models.CharField(max_length=250)
+    answer = models.IntegerField("문제해답")
+
+
+class QuizExample(models.Model):
+    question=models.ForeignKey(QuizQuestion,on_delete=models.CASCADE)
+    no=models.IntegerField("보기번호")
+    content=models.CharField(max_length=250)
+
