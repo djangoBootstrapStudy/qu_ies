7c341275f66c0be40197cb7036ee4a245773b65b
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-16 02:35:56 +0900

    Feat: 퀴즈생성함수 create의 퀴즈문제생성함수 create_question추가

--- make_quiz/views.py
+++ make_quiz/views.py
@@ -10,14 +10,38 @@ from .models import QuizQuestion,QuizExample
 def create_quiz(user,data):
     return Quiz.objects.create(author=user,title=data['title'])
 
+def create_question(quiz,no,data):
+    return QuizQuestion.objects.create(
+        quiz=quiz,
+        no=no,
+        content=data['question_content'],
+        answer=data['answer'],
+    )
+        # {
+        # "message": "success",
+        #     }
+
 def create(request):
     if request.method == "POST":
         user=User.objects.get(pk=1)
         title=request.POST.get('title')
+        question_content=request.POST.get('question')
+
         data={
             'title':title,
+            'question_content':question_content,
+            'answer':3,
         }
+
         quiz=create_quiz(user,data)
+
+        try:
+            no=QuizQuestion.objects.filter(quiz=quiz).count()+1
+        except:
+            if no>10:
+                return render(request,'make_quiz.html',{'message':'문제는 최대 10개까지 만들수 있습니다.'})
+
+        question=create_question(quiz,no,data)
 
         return redirect('/done-qui-es/')
 
