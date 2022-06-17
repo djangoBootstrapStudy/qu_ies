62606da955d5fb1d584bccff242664d1cbf8f2c3
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-18 02:01:01 +0900

    Feat: 퀴즈생성함수 create의 퀴즈문제보기생성함수 create_example 추가

--- make_quiz/views.py
+++ make_quiz/views.py
@@ -22,30 +22,48 @@ def create_question(quiz, no, data):
     #     }
 
 
+def create_example(question, no, data):
+    QuizExample.objects.create(
+        question=question,
+        no=no,
+        content=data[f"example{no}"],
+    )
+    return
+
+
 def create(request):
     if request.method == "POST":
         user = User.objects.get(pk=1)
         title = request.POST.get("title")
         question_content = request.POST.get("question")
+        example1 = request.POST.get("example1")
+        example2 = request.POST.get("example2")
+        example3 = request.POST.get("example3")
+        example4 = request.POST.get("example4")
 
         data = {
             # quiz
             "title": title,
             # question
             "question_content": question_content,
+            # example
+            "example1": example1,
+            "example2": example2,
+            "example3": example3,
+            "example4": example4,
         }
 
+        # 퀴즈
         quiz = create_quiz(user, data)
 
-        try:
-            no = QuizQuestion.objects.filter(quiz=quiz).count() + 1
-        except:
-            if no > 10:
-                return render(
-                    request, "make_quiz.html", {"message": "문제는 최대 10개까지만 만들수 있습니다."}
-                )
+        # 문제
+        qustion_no = QuizQuestion.objects.filter(quiz=quiz).count() + 1
+        question = create_question(quiz, qustion_no, data)
 
-        question = create_question(quiz, no, data)
+        # 보기4개
+        for i in range(4):
+            example_no = QuizExample.objects.filter(question=question).count() + 1
+            create_example(question, example_no, data)
 
         return redirect("/done-qui-es/")
 
