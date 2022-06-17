e8ab29f2891fd3dcad94b7cba4a33067521f8ac0
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-18 02:25:20 +0900

    Refactor: user가 로그인했는지의 여부 확인

--- make_quiz/views.py
+++ make_quiz/views.py
@@ -17,9 +17,6 @@ def create_question(quiz, no, data):
         no=no,
         content=data["question_content"],
     )
-    # {
-    # "message": "success",
-    #     }
 
 
 def create_example(question, no, data):
@@ -33,39 +30,42 @@ def create_example(question, no, data):
 
 def create(request):
     if request.method == "POST":
-        user = User.objects.get(pk=1)
-        title = request.POST.get("title")
-        question_content = request.POST.get("question")
-        example1 = request.POST.get("example1")
-        example2 = request.POST.get("example2")
-        example3 = request.POST.get("example3")
-        example4 = request.POST.get("example4")
+        user = request.user
+        if user.is_authenticated: # 로그인하면
+            title = request.POST.get("title")
+            question_content = request.POST.get("question")
+            example1 = request.POST.get("example1")
+            example2 = request.POST.get("example2")
+            example3 = request.POST.get("example3")
+            example4 = request.POST.get("example4")
 
-        data = {
-            # quiz
-            "title": title,
-            # question
-            "question_content": question_content,
-            # example
-            "example1": example1,
-            "example2": example2,
-            "example3": example3,
-            "example4": example4,
-        }
+            data = {
+                # quiz
+                "title": title,
+                # question
+                "question_content": question_content,
+                # example
+                "example1": example1,
+                "example2": example2,
+                "example3": example3,
+                "example4": example4,
+            }
 
-        # 퀴즈
-        quiz = create_quiz(user, data)
+            # 퀴즈
+            quiz = create_quiz(user, data)
 
-        # 문제
-        qustion_no = QuizQuestion.objects.filter(quiz=quiz).count() + 1
-        question = create_question(quiz, qustion_no, data)
+            # 문제
+            qustion_no = QuizQuestion.objects.filter(quiz=quiz).count() + 1
+            question = create_question(quiz, qustion_no, data)
 
-        # 보기4개
-        for i in range(4):
-            example_no = QuizExample.objects.filter(question=question).count() + 1
-            create_example(question, example_no, data)
+            # 보기4개
+            for i in range(4):
+                example_no = QuizExample.objects.filter(question=question).count() + 1
+                create_example(question, example_no, data)
 
-        return redirect("/done-qui-es/")
+            return redirect("/done-qui-es/") #여기서 return 이면 test가 달라짐
+        else:
+            return render(request, "make_quiz.html",{"message": "로그인을 해주세요"})
 
     else:
         return render(request, "make_quiz.html")
