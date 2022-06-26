64c4efa012b22aa467dc9bbf6b98afd25de00fe6
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-26 23:09:23 +0900

    Refactor: '#todo: 문제하나에 보기 4개 생성 & 답확인' 테스트코드 수정

--- make_quiz/tests.py
+++ make_quiz/tests.py
@@ -100,7 +100,7 @@ class TestView(TestCase):
         self.assertTrue(QuizQuestion.objects.last().no == 1)
         self.assertEqual(QuizQuestion.objects.last().content, content)
 
-    # 문제 하나에 보기 4개 생성후 개수 확인
+    # todo: 문제하나에 보기 4개 생성 & 답확인
     def test_create_four_examples_in_one_question_in_quiz(self):
         # Given
         # 퀴즈생성 & 퀴즈의 문제 생성
@@ -113,85 +113,36 @@ class TestView(TestCase):
             no=self.question_no_001,
             content="내가 좋아하는 계절은?",
         )
+        answer = 1
         # 보기 생성시 필요한 데이터
         data = {
-            "example1": "봄",
-            "example2": "여름",
-            "example3": "가을",
-            "example4": "겨울",
+            "example1": {"no": 1, "content": "봄"},
+            "example2": {"no": 2, "content": "여름"},
+            "example3": {"no": 3, "content": "가을"},
+            "example4": {"no": 4, "content": "겨울"},
+            "answer": {"no": answer},
         }
 
         # When
-        # 문제의 보기 4개 생성
-        for i in range(4):
-            # 보기번호
-            self.example_no = (
-                QuizExample.objects.filter(question=self.question_001).count() + 1
-            )
-            # 보기생성함수
-            create_example(self.question_001, self.example_no, data)
+        create_example(self.question_001, data)
 
         # Then
-        # 퀴즈의 문제가 1개인지 확인
-        # 보기가 4개인지 확인
-        # 마지막으로 만들어진 보기의 번호가 4번인지
-        # 보기 4개중 answer=True인 값이 1개인지 확인
+        """
+        1.퀴즈의 문제가 1개인지 확인
+        2.보기가 4개인지 확인
+        3.보기 4개중 answer=True인 값이 1개인지 확인
+        4.답이 맞는지 확인
+        """
         self.assertEqual(QuizQuestion.objects.filter(quiz=self.quiz_001).count(), 1)
         self.assertEqual(
             QuizExample.objects.filter(question=self.question_001).count(), 4
         )
         self.assertEqual(
-            QuizExample.objects.filter(question=self.question_001).last().no, 4
-        )
-        # self.assertEqual(QuizExample.objects.filter(question=self.question_001,answer=True),1)
-
-    # 문제 하나에 퀴즈 4개의 내용 맞는지 확인 & 정답확인(나중에)
-    def test_check_four_examples_contents_in_one_question_in_quiz(self):
-        # Given
-        self.quiz_001 = Quiz.objects.create(author=self.user, title="test퀴즈입니다.")
-        self.question_no_001 = (
-            QuizQuestion.objects.filter(quiz=self.quiz_001).count() + 1
-        )
-        self.question_001 = QuizQuestion.objects.create(
-            quiz=self.quiz_001,
-            no=self.question_no_001,
-            content="내가 좋아하는 계절은?",
-        )
-        # 보기 생성시 필요한 데이터
-        data = {
-            "example1": "봄",
-            "example2": "여름",
-            "example3": "가을",
-            "example4": "겨울",
-        }
-
-        # When
-        # 문제의 보기 4개 생성
-        for i in range(4):
-            # 보기번호
-            self.example_no = (
-                QuizExample.objects.filter(question=self.question_001).count() + 1
-            )
-            # 보기생성함수
-            create_example(self.question_001, self.example_no, data)
-
-        # Then
-        # 4개 보기의 값이 맞는지 확인
-        self.assertEqual(
-            QuizExample.objects.get(question=self.question_001, no=1).content,
-            data["example1"],
+            QuizExample.objects.filter(question=self.question_001, answer=True).count(),
+            1,
         )
         self.assertEqual(
-            QuizExample.objects.get(question=self.question_001, no=2).content,
-            data["example2"],
-        )
-        self.assertEqual(
-            QuizExample.objects.get(question=self.question_001, no=3).content,
-            data["example3"],
-        )
-        self.assertEqual(
-            QuizExample.objects.get(question=self.question_001, no=4).content,
-            data["example4"],
+            QuizExample.objects.get(question=self.question_001, answer=True).no, answer
         )
 
     # 테스트제목 확인
