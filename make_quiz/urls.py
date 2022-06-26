667274033223252b44bd4e2d69e28ef0371695b3
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-26 18:15:48 +0900

    Feat: 메인페이지 url 추가

--- make_quiz/urls.py
+++ make_quiz/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("you-qui-es/", views.create),
+    path("you-qui-es/", views.create_my_quiz),
     path(
         "done-qui-es/",
         views.done,
     ),
+    path("", views.main),
 ]
