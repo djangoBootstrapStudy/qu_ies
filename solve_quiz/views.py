6421dd86beb03d30108c8190cc617a4ec7e2eb94
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-07-08 23:02:29 +0900

    Refactor: 수정 test_quiz_start_title_and_user_check 통과를 위한 views.py

--- solve_quiz/views.py
+++ solve_quiz/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from my_quiz.models import Quiz
+
 
 # Create your views here.
 def solve_quiz(request, pk):
     # print(pk)
-    return render(request, "solve_quiz/quiz_start.html")
+    quiz = Quiz.objects.get(id=pk)
+    # print(f"퀴즈의 제목: {quiz.title}")
+    # print(f"퀴즈의 출제자: {quiz.author}")
+
+    return render(request, "solve_quiz/quiz_start.html", {"quiz": quiz})
