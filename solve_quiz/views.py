c40bd44591409164ce0e28c5b4fc067bbc1db16c
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-07-09 19:02:20 +0900

    Refactor: 수정 solve_quiz.views(필요없는 주석 제거 & post일경우 필적확인란 다르면 재로드)

--- solve_quiz/views.py
+++ solve_quiz/views.py
@@ -1,6 +1,6 @@
 import random
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from my_quiz.models import Quiz
 
@@ -10,18 +10,35 @@ def random_saying():
         "늦었다고 생각할 때가 진짜 너무 늦었다",
         "내일도 할 수 있는 일을 굳이 오늘 할 필요는 없다",
         "길이 없으면 길을 찾아라, 찾아도 없으면 길을 닦아나가라",
+        "부지런함을 대신할 지름길은 없다",
+        "관찰만으로도 많은 것을 배울수 있다",
+        "오늘 걷지 않으면 내일은 뛰어야 한다",
+        "날마다 새로우며 깊어지고 넓어진다",
+        "그대만큼 사랑스러운 사람을 본 일이 없다",
     ]
     return random.choice(sayings)
 
 
 # Create your views here.
 def solve_quiz(request, pk):
-    # print(pk)
-    quiz = Quiz.objects.get(id=pk)
-    # print(f"퀴즈의 제목: {quiz.title}")
-    # print(f"퀴즈의 출제자: {quiz.author}")
-    saying = random_saying()
+    if request.method == "GET":  # get일경우
+        quiz = Quiz.objects.get(id=pk)
+        saying = random_saying()
 
-    return render(
-        request, "solve_quiz/quiz_start.html", {"quiz": quiz, "saying": saying}
-    )
+        return render(
+            request, "solve_quiz/quiz_start.html", {"quiz": quiz, "saying": saying}
+        )
+    else:  # post일경우
+        """saying: 제시된 랜덤필적확인란, follow_saying: 받아쓴 필적확인란"""
+        saying = request.POST["saying"]
+        follow_saying = request.POST["follow-saying"]
+        # print(f'제시된 필적확인란:{saying}')
+        # print(f'받아쓴 필적확인란:{follow_saying}')
+
+        # Todo: 필적확인란이 일치하면 세션 저장 후 quiz페이지
+        if follow_saying == saying:
+
+
+        # Todo:필적확인란 불일치하면 quiz_start 페이지 redirect
+        else:
+            return redirect(f"/qui-es/{pk}/")
