3c3c26688e30e755630f59acf22758a8106d87b2
halmango <kimby0709@naver.com>
2022-07-13 17:42:50 +0900

    Fix: urlpatterns 수정

--- qui_es/urls.py
+++ qui_es/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from my_quiz.views import main
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
@@ -23,4 +25,5 @@ urlpatterns = [
     path("my-qui-es/", include("my_quiz.urls")),
     path("", include("make_quiz.urls")),
     path("qui-es/", include("solve_quiz.urls")),
+    path("", main, name="main"),
 ]
